52c2daa4b1e489af5bdd510eb92203540d0fdc19
brizism <montielbriseida@gmail.com>
2018-01-29 12:01:00 -0500

    add delete, pick and add handlers

--- src/components/Throwback.js
+++ src/components/Throwback.js
@@ -1,12 +1,63 @@
 import React from 'react';
+import Action from './Action';
+import AddAccomplishment from './AddAccomplishment';
 import AccomplishmentList from './AccomplishmentList';
 
 export default class Throwback extends React.Component {
+  state = {
+    accomplishments: ['hi', 'hello', 'help Me'],
+    selectedAccomplishment: undefined
+  }
+
+  handleDeleteAccomplishments = () => {
+    this.setState(() => ({ accomplishments: [] }));
+  }
+
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.accomplishments.length);
+    const accomplishment = this.state.accomplishments[randomNum];
+    this.setState(() => ({ selectedAccomplishment: accomplishment }));
+    alert(accomplishment)
+  }
+
+  handleAddAccomplishment = (accomplishment) => {
+    this.setState((prevState) => ({ accomplishments: prevState.accomplishments.concat([accomplishment]) }))
+  }
+
+  componentDidMount(){
+    console.log('componentDidMount')
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   render(){
     return (
       <div>
         <h1>Welcome to Throwback</h1>
-        <AccomplishmentList />
+        <Action
+          hasAccomplishments={this.state.accomplishments.length > 0} 
+          handlePick={this.handlePick}
+          />
+        <AccomplishmentList
+          accomplishments={this.state.accomplishments}
+          handleDeleteAccomplishments={this.handleDeleteAccomplishments}
+        
+        />
+        <AddAccomplishment  
+          handleAddAccomplishment={this.handleAddAccomplishment}
+        />
       </div>
     )
   }
