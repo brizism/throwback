3d035b528f3f2c8f69e000b8927e076b272502de
brizism <montielbriseida@gmail.com>
2018-01-29 12:02:21 -0500

    add property to devServer to handle client side routing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
-import Throwback from './components/Throwback';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
