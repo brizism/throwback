34510bbd18b9258a4add9e473a9bea3c21524acb
brizism <montielbriseida@gmail.com>
2018-01-28 20:13:24 -0500

    create Throwback component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import Throwback from './components/Throwback';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
 
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+ReactDOM.render(<Throwback />, document.getElementById('app'))
---
+++ src/components/Throwback.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Throwback extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Welcome to Throwback</h1>
+      </div>
+    )
+  }
+}
